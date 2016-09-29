7bc274c012ff332a162c1938e5b9b9bd44f9aefd
Hugo Branquinho <hugobranq@gmail.com>
2016-09-29 14:45:33 +0100

    Python 3.5 complatible

--- tests/t_simple_binary.py
+++ tests/t_simple_binary.py
@@ -20,6 +20,10 @@ class UnitTest(unittest.TestCase):
         self.kt_handle.open(port=11978)
         self.LARGE_KEY_LEN = 8000
 
+    def tearDown(self):
+        self.kt_handle_http.close()
+        self.kt_handle.close()
+
     def test_set(self):
         self.assertTrue(self.kt_handle_http.clear())
 
